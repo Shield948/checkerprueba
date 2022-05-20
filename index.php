a0d85c93f1efe7604efb5d5d5feaeb806563f691
ierfeioq <102769650+ierfeioq@users.noreply.github.com>
2022-05-20 18:24:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 ///https://api.telegram.org/bot<token>/setwebhook?url=<url>
 
-$botToken = "1472905848:AAHj-wDq3UJKkCL61VnuK7w-zOPxadnKjlI"; // Enter ur bot token
+$botToken = "5261110028:AAEVIZ3eV8UsgW3z2VCyxgbgJJBKDVxa8pc"; // Enter ur bot token
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
